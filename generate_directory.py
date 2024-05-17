2df6664c4af08086b191fcf0ae4ec593d6801cbc
Chuck Garcia <chuckgarcian@gmail.com>
2024-05-16 20:19:38 -0500

    Fix return url logic

--- generate_directory.py
+++ generate_directory.py
@@ -16,7 +16,7 @@ def contains_any_string(string, substrings):
     return any(substring in string for substring in substrings)
 
 current_path = os.getcwd()
-target_dir = "ut-website-md"
+target_dir = "ut-website-md/writings"
 parent_folder_path = os.path.dirname(current_path)
 target_folder_path = os.path.join(parent_folder_path, target_dir)
 includes_path = os.path.join(current_path, "includes")
@@ -32,21 +32,26 @@ def main():
     # Directories to ignore
     ignore_dirs = ['.git', 'img-src']
     # Filename patterns to ignore
-    ignore_file_patterns = ['_dir', 'readme.md']
+    ignore_file_patterns = ['dir.md', 'readme.md']
+    
+    # List of return URL's
+    ret_url = '../index.html'
     
     for root, dir_names, file_names in os.walk(target_folder_path):
         if any(ignore_dir in root for ignore_dir in ignore_dirs):
             continue
-        
+    
         # Open a new index for current working directory (cwd)
         root_base = os.path.basename(root)
-        new_index_path = os.path.join(root, f'{root_base}_dir.md')
+        new_index_path = os.path.join(root, 'dir.md')
         new_index = open(new_index_path, 'w')
         
         f2.seek(0)
         yaml_header = f2.read()
         yaml_header = yaml_header.replace ("[replace]", root_base)
+        yaml_header = yaml_header.replace ("[ret-url]", ret_url)
         new_index.write(yaml_header)
+        ret_url = '../dir.html'
         
         if (len (dir_names) > 1):
             new_index.write("\n# Subdirectories\n")
@@ -56,7 +61,7 @@ def main():
                 readme_path = os.path.join(dir_path, 'readme.md')
                 if file_exists(readme_path):
                     print(f'Subdirectory Path: {dir_path}')
-                    s = f'- [{dir_base}]({dir_base}/{dir_base}_dir.html):'
+                    s = f'- [{dir_base}]({dir_base}/dir.html):'
                     new_index.write (s)
 
                     readme = open(readme_path, 'r')
--- includes/file-h.md
+++ includes/file-h.md
@@ -1,6 +1,3 @@
----
-title: "[replace]"
 return-url: '[ret-url]'
 return-text: '← Return' 
 ---
-
